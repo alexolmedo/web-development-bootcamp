fcbe0b51d37e834954d997cf9f9314cbf2f8e021
Alexander Olmedo <alexander.olmeda@deutschinc.com>
2020-01-14 09:47:52 -0500

    Todo List Code Along

---
+++ Javascript/list.js
@@ -0,0 +1,15 @@
+var todos = ["Buy New Turtle"];
+
+var input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+    if (input === "list") {
+        console.log(todos)
+    } else if (input === "new") {
+        var newTodo = prompt("Enter new Todo");
+        todos.push(newTodo);
+    }
+    input = prompt("What would you like to do?");
+}
+
+console.log("OK, you quit the app");
