f530853f9fb2b46f041a01211c0cde37e8df4eee
Alexander Olmedo <alexander.olmeda@deutschinc.com>
2020-01-20 11:13:23 -0500

    Passing Data to components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -17,15 +17,15 @@ class Counter extends Component {
 
   render () {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(product)}
+          onClick={() => this.handleIncrement({id: 1})}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     )
   }
 
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
