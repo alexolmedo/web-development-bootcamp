b5fabdccedc3aad4be32997b435c197217dcbde9
Alexander Olmedo <alexander.olmedo@devsu.com>
2020-01-28 16:47:42 -0500

    Creating the Color Game Part 3

--- Advanced DOM Manipulation/colorGame.js
+++ Advanced DOM Manipulation/colorGame.js
@@ -7,8 +7,10 @@ var colors = [
   'rgb(255, 0, 255)'
 ]
 var squares = document.querySelectorAll('.square')
-var pickedColor = colors[3]
-var colorDisplay = document.getElementById("colorDisplay")
+var pickedColor = pickColor()
+var colorDisplay = document.getElementById('colorDisplay')
+var messageDisplay = document.querySelector('#message')
+
 colorDisplay.textContent = pickedColor
 
 for (var i = 0; i < squares.length; i++) {
@@ -16,14 +18,28 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i]
 
   // add click listeners to squares
-  squares[i].addEventListener("click", function () {
+  squares[i].addEventListener('click', function () {
     // grab color of clicked square
     var clickedColor = this.style.backgroundColor
     // compare color to pickedColor
-    if(clickedColor===pickedColor){
-      alert("Correct")
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = 'Correct!'
+      changeColors(clickedColor)
     } else {
-      alert("Wrong")
+      this.style.backgroundColor = '#232323'
+      messageDisplay.textContent = 'Try again'
     }
   })
+}
+
+function changeColors (color) {
+  // loop through all squares
+  for (var i = 0; i < colors.length; i++) {
+    squares[i].style.backgroundColor = color
+  }
+}
+
+function pickColor () {
+  var random = Math.floor(Math.random() * colors.length)
+  return colors[random]
 }
