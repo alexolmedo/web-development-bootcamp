86107e86e6da5c2dc3b2d41efd392aafd6543209
Alexander Olmedo <olmedo2408@gmail.com>
2022-03-17 20:05:41 -0500

    Listening to events

--- react/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ react/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const clickedHandler = () => {
+    console.log("Clicked!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,8 +15,9 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickedHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
