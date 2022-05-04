83189cea16c19f4c584df4c5b59ae092012fc9c3
Alexander Olmedo <olmedo2408@gmail.com>
2022-05-04 18:00:36 -0500

    Using one state instead

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,38 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmmount, setEnteredAmmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmmount, setEnteredAmmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
-  const ammountChangeHandler = (event) => {
-    setEnteredAmmount(event.target.value);
+  const ammuntChangeHandler = (event) => {
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
@@ -26,12 +44,12 @@ const ExpenseForm = () => {
           <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Ammount</label>
+          <label>Amount</label>
           <input
             type="number"
             min="0.01"
             step="0.01"
-            onChange={ammountChangeHandler}
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
