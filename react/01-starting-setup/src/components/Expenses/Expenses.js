e71fb6188e1072c0546573879b014435c32a2585
Alexander Olmedo <olmedo2408@gmail.com>
2022-05-17 12:43:35 -0500

    Make the filter work

--- react/01-starting-setup/src/components/Expenses/Expenses.js
+++ react/01-starting-setup/src/components/Expenses/Expenses.js
@@ -7,9 +7,13 @@ import ExpensesFilter from "../NewExpense/ExpensesFilter";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = (filterValue) => {
-    setFilteredYear();
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -18,7 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
